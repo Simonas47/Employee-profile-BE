891be3c048e860ed068c1fb1798a346030452a89
Tautvydas Valašinas <tautvydas.valasinas@devbridge.com>
2023-03-10 13:58:53 +0200

    Add initial project repository

---
+++ src/main/java/com/sourcery/employeeprofile/EmployeeProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sourcery.employeeprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProfileApplication.class, args);
+	}
+
+}
