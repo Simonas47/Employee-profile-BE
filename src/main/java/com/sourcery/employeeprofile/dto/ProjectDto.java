343c2841265990355fb08ffe4cb4ef2eb401e0d7
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-04-03 13:47:39 +0300

    Create new ProjectEmployee and ProjectDto classes

---
+++ src/main/java/com/sourcery/employeeprofile/dto/ProjectDto.java
@@ -0,0 +1,23 @@
+package com.sourcery.employeeprofile.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class ProjectDto {
+    private UUID id;
+    private String title;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+    private List<EmployeeDto> employees;
+}
