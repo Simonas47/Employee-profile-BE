491e38617d45a25095d3db5919f22c2a4c6bc1d1
Jonas <jonasarmalis60@gmail.com>
2023-04-11 00:51:55 +0300

    Initial admin user created

--- src/main/resources/db/migration/V5__createUserTable.sql
+++ src/main/resources/db/migration/V5__createUserTable.sql
@@ -5,3 +5,5 @@ CREATE TABLE IF NOT EXISTS users(
 	email            VARCHAR(255)                            NOT NULL,
 	password         VARCHAR(255)                            NOT NULL
 );
+
+INSERT INTO users (email, password) VALUES ('admin@admin.com', 'admin');
