a255c8a9a7433891b5ca9b1ef4979b3994487f91
Jonas <jonasarmalis60@gmail.com>
2023-04-15 21:13:54 +0300

    Multiple employment dates added to an employee, models modified.

---
+++ src/main/java/com/sourcery/employeeprofile/model/EmploymentDate.java
@@ -0,0 +1,17 @@
+package com.sourcery.employeeprofile.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmploymentDate {
+    private Date hiringDate;
+    private Date exitDate;
+}
