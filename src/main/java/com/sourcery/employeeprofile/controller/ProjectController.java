e634061707c4bf19f9ce6c3c2e4b71bf6fbd1b73
Tautvydas Valašinas <tautvydas.valasinas@devbridge.com>
2023-04-19 15:34:59 +0300

    add additional CrossOrigin exception

--- src/main/java/com/sourcery/employeeprofile/controller/ProjectController.java
+++ src/main/java/com/sourcery/employeeprofile/controller/ProjectController.java
@@ -16,7 +16,7 @@ import static com.sourcery.employeeprofile.EmployeeProfileApplication.BASE_URL;
 
 @RestController
 @RequestMapping(value = BASE_URL + "/project")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://employee-profile.devbstaging.com"})
 public class ProjectController {
     @Autowired
     ProjectService projectService;
