2973baa3f0e8a0c60b6273ea4e612cc1a00145f7
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-04-21 11:08:06 +0300

    Implement SearchSkillDto and required methods

--- src/main/java/com/sourcery/employeeprofile/controller/SkillController.java
+++ src/main/java/com/sourcery/employeeprofile/controller/SkillController.java
@@ -1,5 +1,7 @@
 package com.sourcery.employeeprofile.controller;
 
+import com.sourcery.employeeprofile.dto.EmployeeSkillDto;
+import com.sourcery.employeeprofile.dto.SearchSkillDto;
 import com.sourcery.employeeprofile.dto.SkillDto;
 import com.sourcery.employeeprofile.dto.ChangedSkillsDto;
 import com.sourcery.employeeprofile.service.SkillsService;
@@ -24,5 +26,10 @@ public class SkillController {
     @PutMapping("/update")
     public void updateEmployeeSkill(@RequestBody ChangedSkillsDto changedSkills) {
         skillsService.updateEmployeeSkills(changedSkills);
+    }
+
+    @GetMapping("/getSkillsCategories")
+    public List<SearchSkillDto> getSkillsCategories() {
+        return skillsService.getSkillsCategories();
     }
 }
