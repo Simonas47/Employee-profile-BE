e4fb670623fec428c8d98d2d51739f906cadeee7
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-04-22 22:52:24 +0300

    Change in model types

---
+++ src/main/java/com/sourcery/employeeprofile/dto/DropdownSkillDto.java
@@ -0,0 +1,17 @@
+package com.sourcery.employeeprofile.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class DropdownSkillDto {
+    private UUID skillId;
+    private String skillName;
+}
