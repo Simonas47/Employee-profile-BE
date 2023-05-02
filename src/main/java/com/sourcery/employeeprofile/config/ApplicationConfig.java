6e8566500a565c7815db90f6939972f372c1d242
Jonas <jonasarmalis60@gmail.com>
2023-05-02 19:11:31 +0300

    remote origin added, useless response headers removed, application yml files simplified.

--- src/main/java/com/sourcery/employeeprofile/config/ApplicationConfig.java
+++ src/main/java/com/sourcery/employeeprofile/config/ApplicationConfig.java
@@ -13,7 +13,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://employee-profile.com/", "http://localhost:3000/")
+                .allowedOrigins("https://employee-profile.devbstaging.com/", "http://localhost:3000/")
                 .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name(), HttpMethod.PATCH.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name())
                 .maxAge(86400);
